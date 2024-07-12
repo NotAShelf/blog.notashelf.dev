121edc8a90d974b470ac42c6a8c8264de9e7f96f
NotAShelf <raf@notashelf.dev>
2024-07-12 03:17:38 +0300

    make nix shell reproducible

--- shell.nix
+++ shell.nix
@@ -1,4 +1,12 @@
-{pkgs ? import <nixpkgs> {}}:
+{
+  nixpkgs ?
+    builtins.fetchTarball {
+      name = "nixpkgs-stable";
+      url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz";
+      sha256 = "1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+    },
+  pkgs ? import nixpkgs {config = {};},
+}:
 pkgs.mkShellNoCC {
   buildInputs = builtins.attrValues {inherit (pkgs) jq sassc pandoc;};
 }
