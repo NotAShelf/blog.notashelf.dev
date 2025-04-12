2b6e7687d2f6465f4a4b646a87cbb0c6e08bd677
NotAShelf <raf@notashelf.dev>
2025-04-12 23:47:32 +0300

    meta: redirect to new site

--- templates/scripts/copyright.js
+++
@@ -1,5 +0,0 @@
-// Update the copyright text
-const currentYear = new Date().getFullYear();
-const copyrightYear = currentYear !== 2024 ? "2024 - " + currentYear : "2024";
-
-document.getElementById("copyright").textContent = copyrightYear;
